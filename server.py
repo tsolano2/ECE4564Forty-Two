a3f5523e83e6275eb59348fc94246407ff15903f
tsolano2 <terencio@vt.edu>
2018-02-14 16:01:57 -0500

    Server file

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 from cryptography.fernet import Fernet
 import hashlib
 import sys
-import serverKeys
+#import serverKeys
 import wolframalpha
 import pickle
 
@@ -20,47 +20,52 @@ serverSize = int(sys.argv[6])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#client = wolframalpha.Client(wolfram_alpha_appid)
+m = hashlib.md5()
+
+#wolframClient = wolframalpha.Client(wolfram_alpha_appid)
 
 s.bind(('0.0.0.0',serverPort))
-    
-print('[Checkpoint]: Created Socket at 0.0.0.0 on port '+str(serverPort)) #first checkPoint
+
+print('[Checkpoint] Created Socket at 0.0.0.0 on port '+str(serverPort)) #first checkPoint
 
 s.listen(backLog)
 
-print('[Checkpoint]: Listening for client connections!')
+print('[Checkpoint] Listening for client connections!')
 
       ############################################ProcessServerRequest########################################################
 while 1:
-    
+
     client, address = s.accept()
     addressString = str(address)
-    print('[Checkpoint]: Accepted client connection from '+addressString+' at port '+str(serverPort))
-    
+    print('[Checkpoint] Accepted client connection from '+addressString+' at port '+str(serverPort))
+
     data = client.recv(serverSize)
     stringData = str(data)
-    print('[Checkpoint]: Received: '+stringData)
-    
-    testTuple = ('answerText', 'md5Hash')
-    sendData = pickle.dumps(testTuple)
-    client.send(sendData)
-    s.close()
-    
-    
-    
-    
-    
-    
-    
-    
+    print('[Checkpoint] Received: '+stringData)
 
+    (key, encryptedQuestion, md5Hash) = data
 
+    f = Fernet(key)
 
+    m.update(encryptedQuestion)
 
+    plainQuestion = f.decrypt(encryptedQuestion)
 
-    
-            
-    
-    
-    
-    
+    checkHash = m.digest()
+
+    if(checkHash==md5Hash):
+        print('[Checkpoint] Checksum is VALID')
+        print('[Checkpoint] Decrypt using key '+str(key)+' | plaintext = '+plainQuestion)
+        print('[Checkpoint] Speaking Question: '+plainQuestion)
+        #speak through GTTS
+        print('[Checkpoint] Sending question to Wolfram Alpha: '+plainQuestion)
+        #plainAnswer = wolframClient.query(plainQuestion)
+        #print('Checkpoint] Recieved answer from Wolframalpha: '+plainAnswer)
+        #encryptedAnswer = f.encrypt(plainAnswer)
+        #print('[Checkpoint] Encrypt: Generate key: '+key+' | Ciphertext: '+encryptedAnswer)
+        #m.update(encryptedAnswer)
+        #checkHash = m.digest()
+        #print('[Checkpoint] MD5 Checksum: '+checkHash)
+        #
+    else:
+        print('[Checkpoint] Checksum is INVALID')
