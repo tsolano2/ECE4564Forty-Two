36c98dfa500455fcbd420ae67b67ce4644135d24
aisiadinso1 <35505255+aisiadinso1@users.noreply.github.com>
2018-02-18 17:57:53 -0500

    made comments

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ SERVER_IP = ""
 SERVER_PORT = ""
 SOCKET_SIZE = ""
 HASHTAG = ""
-if sys.argv[1] == "-s":
+if sys.argv[1] == "-s": #This section parses the command line and catches exceptions
     SERVER_IP = sys.argv[2]
     if sys.argv[3] == "-p":
         SERVER_PORT = sys.argv[4]
@@ -37,31 +37,37 @@ question = ""
 text = ""
 key = ""
 
-#From tweepy tutorial
+#From tweepy tutorial 
 class listener(StreamListener):
+<<<<<<< HEAD
+    def on_status(self, status): 
+        text = status.text  #gets the tweet from twitter
+        user = status.user.screen_name  #gets the screen name
+        question = text.replace(HASHTAG, "") 
+=======
     def on_status(self, status):
         #print(repr(status))
         text = status.text
         user = status.user.screen_name
         question = text.replace(HASHTAG, "")
+>>>>>>> 421e8b56d4225254b5e7995cefbe00c2b09e5cac
         print("[Checkpoint] New Tweet: " + question + " | User: " + user)
 
         # Used from cryptography.io
-        key = Fernet.generate_key()
-        #print("Here is the key: %i" (key.decode("utf-8")))
+        key = Fernet.generate_key() #generates the key
         f = Fernet(key)
         question_bytes = question.encode()
         token = f.encrypt(question_bytes)
         offset = 5
         print("[Checkpoint] Encrypt: Generater Key: " + key.decode("utf-8")
               + " | Ciphertext: " + token.decode("utf-8"))
-        #print('Cyphertext: ', cypher_text)
 
         hash_object = hashlib.md5(token)
         print("[Checkpoint] Generated MD5 Checksum: " + hash_object.hexdigest())
 
         tup = (key, token, hash_object.hexdigest())
         print("[Checkpoint] Connecting to " + SERVER_IP + " on port " + SERVER_PORT)
+        
         #from stack overflow
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((SERVER_IP, int(SERVER_PORT)))
@@ -74,8 +80,11 @@ class listener(StreamListener):
         
         #Loads information
         (answerEncrypt, answerCheckSum ) = pickle.loads(info)
+<<<<<<< HEAD
+=======
         #answerEncrypt = tempTup[0]
         #answerCheckSum = tempTup[1]
+>>>>>>> 421e8b56d4225254b5e7995cefbe00c2b09e5cac
         checkHash = hashlib.md5(answerEncrypt)
         
         print("[Checkpoint] Received data: " + str(tempTup))
@@ -86,7 +95,7 @@ class listener(StreamListener):
             decrypt = f.decrypt(answerEncrypt)
             print("[Checkpoint] Decrypt: Using Key " + answerEncrypt.decode("utf-8") + " | Plaintext: " + decrypt.decode("utf-8"))            
         else:
-            raise Exception("[Checkpoint] Checksum is INVALID")
+            raise Exception("[Checkpoint] Checksum is INVALID") #catches error if the checksum is invalid
 
         return True
     def on_error(self, status):
